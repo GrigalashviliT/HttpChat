c79de4fc8f5dd01d063cd91a6df9c3bf042d5e7d
Tornike Grigalashvili <tornike.grigalashvili@twino.ge>
2020-07-27 18:58:40 +0400

    Initial commit

---
+++ app/src/main/java/com/tgrig16/httpchat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tgrig16.httpchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
